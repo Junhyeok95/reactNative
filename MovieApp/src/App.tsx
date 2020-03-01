1a3e6ed8d7292ea84c546eae68371f74c98d13a1
JangJunHyeok <btrya23@gmail.com>
2020-03-01 23:31:46 +0900

    08 MovieApp gk... djfuqek

--- MovieApp/src/App.tsx
+++ MovieApp/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import Navigator from '~/Screens/navigator';
+import Navigator from '~/Screens/Navigator';
 interface Props {}
 
 const App = ( {}: Props ) => {
