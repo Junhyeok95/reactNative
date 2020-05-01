d5333b4915cd9e924cbd07d8523b35189312d81c
JANG JUNHYEOK <btrya23@gmail.com>
2020-05-01 23:58:22 +0900

    Screen2, navi

--- Screen2/src/App.tsx
+++ Screen2/src/App.tsx
@@ -2,39 +2,39 @@ import React, { Fragment, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen'
 import Styled from 'styled-components/native';
+import { UserContextProvider } from '~/Contexts/User';
+import Navigator from '~/Screens/Navigator';
 
 const SafeAreaView = Styled.SafeAreaView`
   flex: 1;
 `;
-const View = Styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-const Text = Styled.Text`
-  font-size: 32px;
-  padding: 8px;
-  border: 1px;
-`;
+// const View = Styled.View`
+//   flex: 1;
+//   justify-content: center;
+//   align-items: center;
+// `;
+// const Text = Styled.Text`
+//   font-size: 32px;
+//   padding: 8px;
+//   border: 1px;
+// `;
 
 interface Props {}
 
 const App = ({ }: Props) => {
 
-  useEffect(() => {
-    console.log("App.tsx useEffect");
-    setTimeout(() => SplashScreen.hide(), 1000);
-  }, []);
+  // useEffect(() => {
+  //   console.log("App.tsx useEffect");
+  //   setTimeout(() => SplashScreen.hide(), 1000);
+  // }, []);
 
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View>
-          <Text>
-            Hello world
-          </Text>
-        </View>
+        <UserContextProvider>
+          <Navigator />
+        </UserContextProvider>
       </SafeAreaView>
     </Fragment>
   );
