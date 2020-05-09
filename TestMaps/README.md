0643e0159938703bfa7d74dc1620faa1253e305b
JANG JUNHYEOK <btrya23@gmail.com>
2020-05-09 16:45:51 +0900

    TestMaps, mpas

--- TestMaps/README.md
+++ TestMaps/README.md
@@ -4,3 +4,6 @@ npm i -S styled-components
 npm i -D typescript @types/react @types/react-native @types/styled-components babel-plugin-root-import
 npm install react-native-maps --save-exact
 ```
+```
+npm i -S @react-native-community/geolocation react-native-geolocation-service
+```
--- TestMaps/src/App.tsx
+++ TestMaps/src/App.tsx
@@ -1,16 +1,66 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {PermissionsAndroid, Platform, Alert}from 'react-native';
 import Styled from 'styled-components/native';
 import MapView, {PROVIDER_GOOGLE, Marker, Polyline} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+
 const SafeAreaView = Styled.SafeAreaView`
   flex: 1;
 `;
-const Text = Styled.Text`
+
+const View = Styled.View`
+  border-color: #00F;
+  border-width: 2px;
   padding: 8px;
-  text-align: center;
-  border-width: 1px;
-  font-size: 24px;
+  margin: 8px;
 `;
+const View2 = Styled.View`
+  border-color: #0AA;
+  border-width: 2px;
+  padding: 8px;
+  margin: 8px;
+`;
+const Text = Styled.Text`
+  text-align: center;
+  font-size: 16px;
+`;
+const TouchableOpacityView = Styled.View`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+// position: absolute;
+// top: 32px;
+// left: 32px;
+const TouchableOpacity = Styled.TouchableOpacity`
+  background-color: #00F;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  justify-content: center;
+  margin-right: 32px;
+`;
+const TouchableOpacity2 = Styled.TouchableOpacity`
+  background-color: #0AA;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  justify-content: center;
+`;
+interface ILocation {
+  latitude: number;
+  longitude: number;
+}
+interface ICoordinate {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  timestamp: number;
+  accuracy: number;
+  speed: number;
+  heading: number;
+  isFromMockProvider: boolean;
+}
 
 interface Props {}
 
@@ -18,56 +68,147 @@ const App = ({ }: Props) => {
   // ACCESS_FINE_LOCATION -> GPS 기반
   // ACCESS_COARSE_LOCATION -> NETWORK 기반
   // ACCESS_BACKGROUND_LOCATION -> 백그라운드
-  const androidPermissionLocation = () => {
-    if (Platform.OS === 'android') { // && Platform.Version >= 23
-      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+
+  const [onMap, setOnMap] = useState<boolean>(false);
+  const [onSave, setOnSave] = useState<boolean>(false);
+  const [location, setLocation] = useState<ILocation | undefined>(undefined);
+  const [marginTop, setMarginTop] = useState<number>(1);
+  const [coordinate, setCoordinate] = useState<ICoordinate | undefined>(undefined);
+
+  const [locations, setLocations] = useState<Array<ILocation>>([]);
+  const [nowSpeed, setNowSpeed] = useState<number>(0);
+  const [nowTimestamp, setNowTimestamp] = useState<number>(0);
+
+  useEffect(() => {
+    console.log("------------------ useEffect ------------------");
+
+    if (Platform.OS === 'android' && Platform.Version >= 23) {
+      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => { // check
         if (result) {
-          console.log("android ACCESS_COARSE_LOCATION ok");
+          // console.log("android LOCATION check OK");
         } else {
-          PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+          PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => { // request
             if (result) {
-              console.log("android ACCESS_COARSE_LOCATION resolve");
+              // console.log("android LOCATION request Ok");
             } else {
-              console.log("android ACCESS_COARSE_LOCATION reject");
+              // console.log("android LOCATION reject");
             }
           });
         }
       });
     }
-  };
 
-  useEffect(() => {
-    androidPermissionLocation();
-    console.log("--- useEffect");
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+        });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+
+    return () => {
+
+    };
   }, []);
-
-  const [data, setData] = useState<any>();
 
   return (
     <SafeAreaView>
-      <MapView
-        style={{flex:1}}
-        provider={PROVIDER_GOOGLE}
-        loadingEnabled={true}
+      <TouchableOpacityView>
+        <TouchableOpacity
+          onPress={()=>{
+            setLocations([]);
+            setNowSpeed(0);
+            setNowTimestamp(0);
+          }}
+        >
+          <Text
+            style={{color:"#FFFFFF"}}
+          >
+            초기화
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity2
+          onPress={()=>{
+            setOnSave(!onSave);
+          }}
+        >
+          <Text
+            style={{color:"#FFFFFF"}}
+          >
+            {onSave?"중지":"기록"}
+          </Text>
+        </TouchableOpacity2>
+      </TouchableOpacityView>
+      {location && (
+        <MapView
+          style={{flex:1, marginTop}}
+          loadingEnabled={true}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          onMapReady={() => {
+            setMarginTop(0)
+            setOnMap(true)
+          }}
+          showsMyLocationButton={true}
+          showsCompass={false}
 
-        showsUserLocation={true}
-        showsMyLocationButton={false}
-        showsPointsOfInterest={false}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.02,
+            longitudeDelta: 0.02,
+          }}
 
-        initialRegion={{
-          latitude: 35.896311,
-          longitude: 128.622051,
-          latitudeDelta: 0.02,
-          longitudeDelta: 0.02,
-        }}
-        onUserLocationChange={ e => {
-          console.log(e.nativeEvent.coordinate);
-          setData(JSON.stringify(e.nativeEvent.coordinate));
-        }}
-      />
-      <Text>
-        {data}
-      </Text>
+          onRegionChangeComplete={region => {
+            console.log(region);
+          }}
+
+          onUserLocationChange={ e => {
+            if(onSave){
+              setCoordinate({
+                latitude: e.nativeEvent.coordinate.latitude,
+                longitude: e.nativeEvent.coordinate.longitude,
+                altitude: e.nativeEvent.coordinate.altitude,
+                timestamp: e.nativeEvent.coordinate.timestamp,
+                accuracy: e.nativeEvent.coordinate.accuracy,
+                speed: e.nativeEvent.coordinate.speed,
+                heading: e.nativeEvent.coordinate.heading,
+                isFromMockProvider: e.nativeEvent.coordinate.isFromMockProvider,
+              });
+              const {latitude, longitude} = e.nativeEvent.coordinate;
+              setLocations([...locations, {latitude, longitude}]);
+              const {speed} = e.nativeEvent.coordinate;
+              const {timestamp} = e.nativeEvent.coordinate;
+              setNowSpeed(parseFloat(speed.toFixed(3)));
+              setNowTimestamp(timestamp);
+            }
+            console.log(e.nativeEvent.coordinate);
+          }}
+        >
+          {onSave && (<Polyline
+            coordinates={locations}
+            strokeWidth={2}
+            strokeColor="#00F" 
+          />)}
+        </MapView>
+      )}
+      <View>
+        <Text>
+        {JSON.stringify(coordinate)}
+        </Text>
+      </View>
+      <View2>
+        <Text>
+          save {locations.length} : {}/ speed : {nowSpeed} / time : {nowTimestamp}
+        </Text>
+      </View2>
+      
     </SafeAreaView>
   );
 };
